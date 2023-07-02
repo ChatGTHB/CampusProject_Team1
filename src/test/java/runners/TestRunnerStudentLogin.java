5186897dc9d08228ad25bff3475042f3d5d1ba09
ChatGTHB <cihat.kose@hotmail.com>
2023-07-02 03:01:41 +0300

    Added to get cucumber report in Jenkins

--- src/test/java/runners/TestRunnerStudentLogin.java
+++ src/test/java/runners/TestRunnerStudentLogin.java
@@ -1,12 +1,13 @@
 package runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+        import io.cucumber.testng.AbstractTestNGCucumberTests;
+        import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles/StudentLogin.feature"},
         glue = {"StepDefinitions"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber/cucumber.json"},
+        dryRun = false
 )
 public class TestRunnerStudentLogin extends AbstractTestNGCucumberTests {
 }
